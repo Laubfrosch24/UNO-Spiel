d53bf94e3ef34c596059c987292240576c3a3720
Grohedal <pascal.steingraeber@outlook.de>
2018-01-15 20:47:03 +0100

    sadasd
    dfsfsf

--- UNO/Program.cs
+++ UNO/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UNO.Model;
 
 namespace UNO
 {
@@ -12,6 +13,7 @@ namespace UNO
     {
         const int HttpPort = 1337;
         const int WebSocketPort = 666;
+        static List<Spieler> AllSpieler = new List<Spieler>();
 
         static void Main(string[] args)
         {
@@ -19,11 +21,21 @@ namespace UNO
             WebSocketServer wss = new WebSocketServer($"ws://0.0.0.0:{WebSocketPort}");
             wss.Start(socket => {
                 socket.OnOpen = () => socket.Send("Halloasdasdasd");
+                socket.OnOpen = () => NewSpieler(socket);
             });
 
 #if DEBUG
             Process.Start($"http://localhost:{HttpPort}");
 #endif
+        }
+
+        private static void NewSpieler(IWebSocketConnection socket)
+        {
+            AllSpieler.Add(new Spieler("asdasd", socket));
+            if(AllSpieler.Count >= 2)
+            {
+                Spielfeld spielfeld = new Spielfeld(AllSpieler);
+            }
         }
     }
 }
